c5178ee592639e3835016df60689998fb136eace
Aldo Jara <aldo_psn@hotmail.com>
2026-08-02 19:10:12 -0500

    feat: add sitemap and robots configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://aldoalvolante.com',
+  integrations: [sitemap()],
 });
+
